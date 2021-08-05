d726994359d03b1dd6f03161b72554aa21d5a8e7
Parag Negi <parag.negi1999@gmail.com>
2021-08-05 18:29:10 +0530

    Connected Firebase and created Home Page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0uy-YeXr0-ATFxmWIQH0JkIskE4_DddI",
+  authDomain: "clone-83371.firebaseapp.com",
+  projectId: "clone-83371",
+  storageBucket: "clone-83371.appspot.com",
+  messagingSenderId: "448897247066",
+  appId: "1:448897247066:web:1f9c47d8a4a83dd225f563",
+  measurementId: "G-8F1TKL9HMS"
+};
