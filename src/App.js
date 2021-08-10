8f74e0d25925b63812a797921df7f54bbc727317
Parag Negi <parag.negi1999@gmail.com>
2021-08-10 18:44:10 +0530

    Checkout Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
-import Header from "./Header.js";
-import Home from "./Home.js";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
     <div className="app">
+      <Header/>
       <Switch>
 
         <Route path="/checkout">
-          <Header/>
+
           <Checkout/>
         </Route>
 
         <Route path="/">
-          <Header/>
           <Home/>
         </Route>
 
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,1 +1,24 @@
-1:38:50
+import React from 'react';
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+      <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Xiaomi/ILM/HDFC_1500/D21342770_WLD_Mi_Redmi_Note10ProMax_PC_640x45._CB644988387_.jpg"></img>
+
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+
+    </div>
+  )
+}
+
+export default Checkout;
